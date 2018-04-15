e5ffc944871d8e364a27e15445171ba7b72a1b83
Luoxionghui <code_luo@126.com>
2018-04-15 12:47:18 +0800

    my thrid commit 1247

--- main.c
+++ main.c
@@ -7,8 +7,7 @@ int main(int argc, char* argc[])
 	printf("hello world!\n");
 	if(check == 1)
 	{
-		printf("it's test\n");
-		printf("it's test 2\n");
+		printf("hello world2\n");
 	}
 	return 0;
 }
